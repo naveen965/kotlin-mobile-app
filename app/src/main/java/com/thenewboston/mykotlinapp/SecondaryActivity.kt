47e24632cb7ea8aae944edc06ba5df0f795f8c62
Naveen <naveendevinda163@gmail.com>
2019-10-23 21:36:31 +0530

    connected to second activity

---
+++ app/src/main/java/com/thenewboston/mykotlinapp/SecondaryActivity.kt
@@ -0,0 +1,13 @@
+package com.thenewboston.mykotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class SecondaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary)
+    }
+}
